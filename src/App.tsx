047cb53ddedcbad30f4a09a9729f2ed4895bd177
Samys Molina <samyjes4@gmail.com>
2024-10-09 14:49:46 -0400

    config, navbar welcome

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* import { useState } from 'react' */
+
+import './App.css'
+
+function App() {
+ 
+
+  const name = "soy tu nueva interfaz de navegación";
+  return <p>Hola, {name}</p>
+} 
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import Welcome from './welcome.tsx'
+import Navbar from './components/navbar.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Navbar />
+    <App />
+    <Welcome />
+  </StrictMode>,
+)
